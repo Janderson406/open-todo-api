b73ab3a32de43d57ede3f15be32c2bd364533090
Jon Anderson <jondanderson@gmail.com>
2016-06-15 23:17:08 -0600

    Add User authorization, check indexes

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -4,4 +4,15 @@ class ApiController < ApplicationController
   def authenticated?
     authenticate_or_request_with_http_basic {|username, password| User.where( username: username, password: password).present? }
   end
+
+  def current_user
+    current_user = User.find_by(username: username, password: password)
+  end
+
+  def authorize_user
+    list = List.find(params[:id])
+    unless current_user == list.user
+      render json: { error: "Not Authorized", status: 403 }, status: 403
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     resources :users do
-      resources :lists, except: [:new, :edit]
+      resources :lists
     end
 
     resources :lists, only: [] do
       resources :items, only: [:create, :update]
     end
 
-    resources :items, only: [:destroy]
+    resources :items, only: [:destroy, :index]
   end
 end
 # namespace separates the API routes from the rest of the application routes.
